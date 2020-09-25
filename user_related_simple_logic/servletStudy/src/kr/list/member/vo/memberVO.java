af79b45275437c4d5d400b335bb08a4cfe107990
= <=>
2020-09-25 15:30:25 +0900

    1차 완료

---
+++ user_related_simple_logic/servletStudy/src/kr/list/member/vo/memberVO.java
@@ -0,0 +1,38 @@
+package kr.list.member.vo;
+
+import java.sql.Date;
+
+public class memberVO {
+	private String id;
+	private String pw;
+	private String name;
+	private String dates;
+	
+
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDates() {
+		return dates;
+	}
+	public void setDates(String dates) {
+		this.dates = dates;
+	}
+	
+	
+}
